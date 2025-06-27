8b5b4f969507ae05d5df38c3367844342a65698b
chaubeyjaya <jayachaubey03@gmail.com>
2025-06-27 16:48:04 +0530

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import userRoutes from './routes/userRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 50001;
 
 // Connect to MongoDB
 connectDB();
